e81e23c6e73e3f76b77b86cbe00bc22bd9e9ca63
mark <markalessi@fullsail.edu>
2013-01-25 00:02:06 -0500

    new commit

--- alessi_mark_assignment3.js
+++ alessi_mark_assignment3.js
@@ -14,7 +14,8 @@ var meal = [ "breakfast", "lunch", "dinner" ]
 
 //output
 
-console.log("My name is " + name + " and it is now time for " + meal[1] + "! " + " It is " + hungry + " that I am hungry!" );
+console.log("My name is " + name + " and it is now time for " + meal[1] + "! ");
+console.log("Yes, it is " + hungry + " that I am hungry!" );
 console.log("My favorite food is definately " + favFood );
 console.log("My top " + mySand + " favorite types of sandwiches are: " + favTypes);
 
