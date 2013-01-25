b56659318a4f5b3c744bcf6405dca664a033f111
mark <markalessi@fullsail.edu>
2013-01-24 23:07:00 -0500

    update

--- alessi_mark_assignment3.js
+++ alessi_mark_assignment3.js
@@ -2,85 +2,25 @@
 // Project 3 
 // Mark Alessi
 
-var arr = [true, false, "", [], function(){}, 101];
 
-var student = {
-	id: "00000111",
-	name: "Mike",
-	interests: [
-		"JavaScript",
-		"HTML",
-		"CSS",
-		"Flash"
-	],
-	age: 503423,
-	sayHi: function () {
-			console.log("Hi");
-	}
-};
+//var
 
-var key = "age";
+var name = "Mark";
+var favFood = "Sandwiches!";
+var favTypes = ["Italian Sub", "Reuben Sandwich", "Cuban Sandwich"];
+var mySand = 3;
+var hungry = true; 
+var meal = [ "breakfast", "lunch", "dinner" ]
 
-console.log( student[key] );
-console.log( student["interests"] );
-console.log( student.interests );
+//output
+
+console.log("My name is " + name + " and it is now time for " + meal[1] + "! " + " It is " hungry " + that I am hungry!" );
+console.log("My favorite is definately " + favFood );
+console/log("I have " + mySand + " favorite types of Sandwiches:" + favTypes);
+
+//json
 
 
-student.sayHi() ;
-
-student.sayHi = function () {
-	console.log("Get lost");
-};
-
-student["sayHi"]();
-
-for (var key in student) {
-	console.log("key: " + key + ", value: " + student [key]);
-};
-
-
-var handleData = function (json) {
-	for (var i = 0; i < json.friends.length; i++) {
-		var friend = json.friends[i];
-		console.log("User ID: " + friend.uuid + ", Name: " + friend.name + ", Age: " +
-friend.age);
-	};
-};
-
-var cargoHold = [];
-var crew =[];
-var cargoLoad = function (item) {
-	if (item !== "elephants") {
-		cargoHold.push(item);
-	} else {
-		console.log("Not allowed to load elephants!");
-	}
-};
-
-var cargoList = function ()	{
-	for (var n = 0; n < cargoHold.length; n++) {
-		console.log(name + " cargo " + n + " : " +
-			cargoHold[n] + ".");
-		}
-
-	}; 
-	var getName = function () { return name; {
-		return {  
-		"name": getName,
-		"cargoLoad": cargoLoad,
-		"cargoList": cargoList
-	};
-};
-var dauntless  = pirateShip("Dauntless");
-duantless.cargoLoad("oranges");
-duantless.cargoLoad("elephants");
-duantless.cargoList();
-console.log(dauntless.name()); //getter 
-console.log(dauntless);
-
-	
-
-}
 
 
 
