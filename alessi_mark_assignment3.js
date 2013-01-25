7878673635f1fc262227c6f672a28d69a09cf8e5
mark <markalessi@fullsail.edu>
2013-01-24 23:19:25 -0500

    fixed errors

--- alessi_mark_assignment3.js
+++ alessi_mark_assignment3.js
@@ -7,16 +7,16 @@
 
 var name = "Mark";
 var favFood = "Sandwiches!";
-var favTypes = ["Italian Sub", "Reuben Sandwich", "Cuban Sandwich"];
+var favTypes = ["Italian Sub" , " Reuben Sandwich" , " Cuban Sandwich" ];
 var mySand = 3;
 var hungry = true; 
 var meal = [ "breakfast", "lunch", "dinner" ]
 
 //output
 
-console.log("My name is " + name + " and it is now time for " + meal[1] + "! " + " It is " hungry " + that I am hungry!" );
-console.log("My favorite is definately " + favFood );
-console/log("I have " + mySand + " favorite types of Sandwiches:" + favTypes);
+console.log("My name is " + name + " and it is now time for " + meal[1] + "! " + " It is " + hungry + " that I am hungry!" );
+console.log("My favorite food is definately " + favFood );
+console.log("My top " + mySand + " favorite types of sandwiches are: " + favTypes);
 
 //json
 
