e196fedd00968a99539ef60ccb11c36b69e85c80
mark <markalessi@fullsail.edu>
2013-01-31 21:20:31 -0500

    added second js sheet and updated first

--- alessi_mark_assignment4.js
+++ alessi_mark_assignment4.js
@@ -1,3 +1,4 @@
+
 // String and toUpperCase function
 // The string given by user --> prompt to enter name 
 // This is a built in JS function which converts the specified string into all CAPITAL letters!! 
@@ -8,9 +9,9 @@ console.log("Hello " + name.toUpperCase() + " !" )
  
 var areaCode = prompt("Please enter your area code")  
 
-var phoneNumber = prompt("Plese enter your phone number")
+var phoneNumber = prompt("Plese enter your phone number - Example: 987-6543 ")
 
-console.log("Your phone number is (" + areCode + ")" + phoneNumber);
+console.log("Your phone number is (" + areaCode + ")" + phoneNumber);
 
 
 // Pay calculator 
@@ -29,6 +30,9 @@ var otPay
 var regHours 
 
 
+// in my previous JavaScript class I used 
+
+
 status = (work < 40) ? regPay = work * rate: regPay = rate * 40   
 status = (work > 40) ? ot = work - 40 : ot = 0 
 status = (work > 40) ? otPay = regPay * rate * 1.5 : otPay = 0
@@ -42,4 +46,20 @@ console.log("You have worked " + ot + " hours of overtime this week." )
 console.log("Your overtime pay rate is $ " + rate * 1.5 + ".00 per hour")
 console.log("Your additional overtime pay is $ " + otRate + ".00 " )
 console.log("Your total Earnings are: $" + totalPay + ".00")
+
+
+var favFood = "Sandwiches!";
+var favTypes = ["Italian Sub" , " Reuben Sandwich" , " Cuban Sandwich" , "Meatball Sub" , "Roast Beef" ];
+var mySand = 3;
+var hungry = true; 
+var meal = [ "breakfast", "lunch", "dinner", "snack" ]
+
+
+//output
+
+console.log("My name is " + name + " and it is now time for " + meal[1] + "! ");
+console.log("Yes, it is " + hungry + " that I am hungry!" );
+console.log("My favorite food is definately " + favFood );
+console.log("My top " + mySand + " favorite types of sandwiches are: " + favTypes[0] + "," + favTypes[1] + ", and " + favTypes[2]);
+
 
