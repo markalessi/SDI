c1f344e06d8890ad0419adc58bd69d4bd88eb7b6
mark <markalessi@fullsail.edu>
2013-02-14 22:30:28 -0500

    a lot of tweaks code works
    should be next to last commit

--- alessi_mark_assignment2.js
+++ alessi_mark_assignment2.js
@@ -4,92 +4,62 @@
 // Project 2
 
 // Disclaimer: While the story and code for this project is 
-// my sole idea, the concept (SpongeBob Square Pants) is not mine.
+// my sole idea, the concept (SpongeBob Square Pants) and show themes are not mine.
 // I referenced the site http://spongebob.wikia.com/wiki/The_SpongeBob_SquarePants_Wiki
-// for factual information on this fictional program. Thank you! 
+// for some factual information on this fictional program. Thank you! 
 
+// proceduce -- argument is not returned only output to console
+// global variables
 
-// procedure example *** This is like a function but it does not return a value
+var jellyFishPer = 3;
+var jarsNeeded = 3;
 
-// var add = function(firstNumber,secondNumber){
-// var outcome = firstNumber/secondNumber;
-
-// console.log(outcome);
-// };
-
-// division(10,5);
-
-
-
-// main code (for this example)
-
-// conditional within function example
-
-var division = function(firstNumber,secondNumber){
-	if (secondNumber === 0) {
-		return("Can't divide by zero!");
-	} else {
-		var outcome = firstNumber/secondNumber;
-		return "The answer is " + outcome;
-	}
-	
-};
-
-// main code (for this example)		
-
-// var divOutcome = division(10,0)
-// console.log(divOutcome);
-
-// divOutcome = division(20,2);
-// console.log(divOutcome);
-// proceduce -- argument is not returned only output to console.log
-
-function SbGreet(){
-
+function sbGreet(){
 console.log("Hello! I'm SpongeBob Square Pants! Hahaha! Nice to meet you!");  
+console.log("Uh, Hello, Uh, this is Patrick... I think");	
 };
 
-SbGreet();
-var  jellyFishPer = 3
-var  jarsNeeded = 10
+sbGreet();
 
-
-// function -- outcome is passed back to main code
+// number function -- outcome is passed back to main code
 
 var multiply = function(jellyFishPer,jarsNeeded){
 	var totalJellyFish = jellyFishPer * jarsNeeded;
 
-	return(totalJellyFish);
+	return(totalJellyFish); // local variable 
 };
+// main code 
 
-multiply(jellyFishPer,jarsNeeded);
+var multiplyOutcome = multiply(jellyFishPer,jarsNeeded);
 
-var multiplyOutcome = multiply(3,10);
 
 console.log("It's time to go Jelly Fishing!")
 console.log("We need to catch " + jellyFishPer + " jelly fish for every jar of jelly needed.")
 console.log("Mr. Krabs wants " + jarsNeeded + " jars of jelly!")
-console.log("Oh, tarter sauce! That means we need to catch a total of " + totalJellyFish + " jelly fish!")
+console.log("Oh, tarter sauce! That means we need to catch a total of " + multiplyOutcome + " jelly fish!")
+
 //while-loop
 
 var jellyFish = 0; //iterator, counter
-while (jellyFish < 3){
-console.log(jellyFish + " jelly fish caught.");
+while (jellyFish < multiplyOutcome){
+console.log(jellyFish + " jelly fish caught."); //true output 
 jellyFish++;
+if (jellyFish === multiplyOutcome){
+console.log("Yay! All " + jellyFish + " jelly fish caught!"); // false output	
+}
 };
 
-console.log("All " + jellyFish + " jelly fish caught!");
 
 // for-loop ** repeated for correct grammar
 
 for (var krabbyPatty = 3; krabbyPatty > 1; krabbyPatty--) {
-	console.log("You need to make " + krabbyPatty + " more krabby patties.");
+	console.log("I need to make " + krabbyPatty + " more krabby patties.");
 };
-	console.log("You need to make " + krabbyPatty + " more krabby patty.");
+	console.log("I need to make " + krabbyPatty + " more krabby patty.");
 
 for (var krabbyPatty = 1; krabbyPatty > 0; krabbyPatty--) {
 };
-console.log("You've made all the krabby patties!");
+console.log("I've made all the krabby patties!");
 
 // array
 
