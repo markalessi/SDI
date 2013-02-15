04041e9ec210eac145d1dea7d75d8326bcec4f74
mark <markalessi@fullsail.edu>
2013-02-15 00:01:11 -0500

    final commit

--- alessi_mark_assignment2.js
+++ alessi_mark_assignment2.js
@@ -8,15 +8,19 @@
 // I referenced the site http://spongebob.wikia.com/wiki/The_SpongeBob_SquarePants_Wiki
 // for some factual information on this fictional program. Thank you! 
 
-// proceduce -- argument is not returned only output to console
+// main code
 // global variables
 
 var jellyFishPer = 3;
 var jarsNeeded = 3;
+// array string
+var krabbyPattyRecipe = [" crab meat patty", " sea lettuce", " sea onion", " sea tomato", 
+" sea pickles", " sea mustard", " sea ketchup", " cheese"];
+// proceduce -- argument is not returned only output to console
 
 function sbGreet(){
 console.log("Hello! I'm SpongeBob Square Pants! Hahaha! Nice to meet you!");  
-console.log("Uh, Hello, Uh, this is Patrick... I think");	
+// console.log("Uh, Hello, Uh, this is Patrick... I think");	
 };
 
 sbGreet();
@@ -28,10 +32,20 @@ var multiply = function(jellyFishPer,jarsNeeded){
 
 	return(totalJellyFish); // local variable 
 };
+
+// var division = function(firstNumber,secondNumber){
+// if (secondNumber === 0) {
+// return("Can't divide by zero!");
+//	} else {
+//	var outcome = firstNumber/secondNumber;
+//	return "The answer is " + outcome;
+//	}
+	
+//  };
+
 // main code 
 
 var multiplyOutcome = multiply(jellyFishPer,jarsNeeded);
-
 
 console.log("It's time to go Jelly Fishing!")
 console.log("We need to catch " + jellyFishPer + " jelly fish for every jar of jelly needed.")
@@ -40,31 +54,24 @@ console.log("Oh, tarter sauce! That means we need to catch a total of " + multip
 
 //while-loop
 
-var jellyFish = 0; //iterator, counter
+var jellyFish = 1; //iterator, counter
 while (jellyFish < multiplyOutcome){
-console.log(jellyFish + " jelly fish caught."); //true output 
+console.log(jellyFish + " jelly fish have been caught!"); //true output 
 jellyFish++;
 if (jellyFish === multiplyOutcome){
 console.log("Yay! All " + jellyFish + " jelly fish caught!"); // false output	
 }
 };
+// string function 
 
+var krustyKrabOpen = function(work,multiplyOutcome){
+	var work = "it's time to go to work!";
+return work;
 
-// for-loop ** repeated for correct grammar
-
-for (var krabbyPatty = 3; krabbyPatty > 1; krabbyPatty--) {
-	console.log("I need to make " + krabbyPatty + " more krabby patties.");
+console.log("Now that we caught " + multiplyOutcome + " jelly fish " + work);
 };
-	console.log("I need to make " + krabbyPatty + " more krabby patty.");
 
-for (var krabbyPatty = 1; krabbyPatty > 0; krabbyPatty--) {
-};
-console.log("I've made all the krabby patties!");
-
-// array
-
-var krabbyPattyRecipe = [" crab meat patty", " sea lettuce", " sea onion", " sea tomato", 
-" sea pickles", " sea mustard", " sea ketchup", " cheese"];
+// array functions
 
 // push function
 
@@ -74,19 +81,25 @@ krabbyPattyRecipe.push(" the secret ingredient");
 
 krabbyPattyRecipe.unshift(" a seaweed bun");
 
-console.log (krabbyPattyRecipe);
+// console.log (krabbyPattyRecipe);
+
+console.log("Here is a list of ingredients I need to make a delicious Krabby Patty!")
 
 // array for-loop
+
 for (var i=0, k=krabbyPattyRecipe.length; i < k; i++) {	
 	console.log(krabbyPattyRecipe[i]);
 };
 
+// for-loop 
 
-// array for each-loop 
-// for each (var krabby in krabbyPattyRecipe) {
-// console.log(krabby);
-// };
+for (var krabbyPatty = 3; krabbyPatty > 1; krabbyPatty--) {
+	console.log("I need to make " + krabbyPatty + " more krabby patties.");
+};
+	console.log("I need to make " + krabbyPatty + " more krabby patty.");
 
-// function
+// repeated for-loop for correct grammar 
 
-var bikiniBottom 
+for (var krabbyPatty = 1; krabbyPatty > 0; krabbyPatty--) {
+};
+console.log("I've made all the krabby patties!");
