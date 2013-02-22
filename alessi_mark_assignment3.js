b3303844abd0435af8d3974062cf722416a1bdaa
mark <markalessi@fullsail.edu>
2013-02-21 20:26:54 -0500

    json data

--- alessi_mark_assignment3.js
+++ alessi_mark_assignment3.js
@@ -3,9 +3,14 @@
 // SDI 1302
 // Project 3
 
-//object
+// JSON
+
+
+// object
 
 var citizen = { 
+    "characters": {  
+	  "citizen1": { 
 	name: "Spongebob Square Pants",
 	lives: "in a pineapple",
 	interests: "blowing bubbles",
@@ -16,25 +21,49 @@ var citizen = {
 		 "Mr.(Eugene) Krabs",
 		 "Larry the Lobster"
 	], 
-
-// function whosWho 
-
-	whosWho: function (){
-		
-// function with string concatonation
- 
-		console.log(citizen.name + " lives " + citizen.lives + " under the sea."); 
-		console.log(citizen.name +  "'s interests are " + citizen.interests + ".");
-		console.log(citizen.name + " is friends with " + citizen.friends + "."); 
+},
+ "citizen2": {
+	name: "Patrick Star",
+	lives: "under a rock",
+	interests: "blowing bubbles",
+	friends: [
+		"Spongebob Square Pants",
+		 "Sandy Cheecks",
+		 "Squidward Tenticles",
+		 "Larry the Lobster"	 
+	], 
+		}
 	}
-	
+};	
+var citizen 	
+// function with string concatonation
+// function bio 
+
+//bio: function (){
+ 
+		console.log(citizen.characters.citizen1.name + " lives " + citizen.characters.citizen1.lives + " under the sea."); 
+		console.log(citizen.characters.citizen1.name +  "'s interests are " + citizen.characters.citizen1.interests + ".");
+		console.log(citizen.characters.citizen1.name + " is friends with: " + citizen.characters.citizen1.friends + "."); 
+
+var key = "name";
+
+
+// calls function bio
+
+//citizen.characters.citizen1.bio();
+
+//  for (var key in citizen) {
+//	console.log( key + ": " + citizen[key]);
+
+// for-loop 
+var hoursToWork
+for (var hoursToWork = 3; hoursToWork > 1; hoursToWork--) {
+	console.log(citizen.characters.citizen1.name + " has " + hoursToWork + " more hours until he has to work.");
 };
+	console.log(citizen.characters.citizen1.name + " has " + hoursToWork + " more hour until he has to work.");
 
-console.log(citizen);
-console.log(citizen.name);
-console.log(citizen["lives"]);
+// repeated for-loop for correct grammar 
 
-// calls function whosWho
-
-citizen.whosWho();
-
+for (var hoursToWork = 1; hoursToWork > 0; hoursToWork--) {
+};
+console.log("It's time for " + citizen.characters.citizen1.name +  " to go to work!");
