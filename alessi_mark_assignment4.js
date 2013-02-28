c515d45bc912ac2dc5e9203f3dd846d3e750ed07
mark <markalessi@fullsail.edu>
2013-02-28 09:50:15 -0500

    added #1

--- alessi_mark_assignment4.js
+++ alessi_mark_assignment4.js
@@ -5,3 +5,17 @@
 // Project 4
 // Function Library
 
+// var myLibrary = function() {
+
+// #1 - Format a number to use a specific number of decimal places,
+// as for money: 2.1 → 2.10 
+
+function decimalSpots(numberUsed) {
+    var number = numberUsed
+    number.toFixed(2)
+    return console.log("#1 - Format a number to use a specific number of decimal places, as for money: $" + number.toFixed(2));
+    }
+
+
+decimalSpots(75);  // called function // 75 dollars
+
